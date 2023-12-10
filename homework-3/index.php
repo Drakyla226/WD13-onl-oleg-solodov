e6b30f13ab5146fed19fe94feb1c5f50338d5847
Drakyla226 <www.drakyla226@mail.ru>
2023-12-10 21:47:21 +0300

    homework-3

---
+++ homework-3/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Третье</title>
+</head>
+
+<body>
+    <section class="tabs">
+        <div class="section-tabs">
+            <div class="tabs-number number__one">
+                <div class="tabs-number__page">
+                    <h5 class="tabs-numbers">TAB 1</h5>
+                </div>
+            </div>
+
+            <div class="tabs-number">
+                <div class="tabs-number__page">
+                    <h5 class="tabs-numbers">TAB 2</h5>
+                </div>
+            </div>
+            <div class="tabs-number">
+                <div class="tabs-number__page">
+                    <h5 class="tabs-numbers">TAB 3</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-tabs_one">
+            <div class="tabs-container__discr">
+                <h3 class="tabs-number__h3">Tabs with soft transitioning effect.</h3>
+                <h5 class="tabs-number__h5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod<br>
+                    bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra<br>
+                    justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque<br>
+                    penatibus et magnis dis parturient montes.<br>
+                    <br>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod<br>
+                    bibendum laoreet.
+                </h5>
+                <div class="tabs-number__link">
+                    <a class="link-download" href="#">Download</a>
+                </div>
+            </div>
+            <div class="tabs-container__img">
+                <img src="assets/img/statue.jpg" alt="Статуя свободы">
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
